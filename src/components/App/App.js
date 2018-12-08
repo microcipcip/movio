b711c53a9e93715116e22c3bf283297a79f0b8d8
Salvatore Tedde <microcipcip@gmail.com>
2018-12-08 16:17:09 +0000

    Adding base styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import { ThemeProvider } from 'styled-components'
+import Styles from 'styles'
+import { theme } from 'styles/vars'
 
 class App extends Component {
   render() {
-    return <div className="App">App</div>
+    return (
+      <ThemeProvider theme={theme.dark}>
+        <React.Fragment>
+          <Styles />
+          <div>App</div>
+        </React.Fragment>
+      </ThemeProvider>
+    )
   }
 }
 
