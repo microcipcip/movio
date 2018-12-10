89d9318320e95c637c7959a70eb6fa5e701a5791
Salvatore Tedde <microcipcip@gmail.com>
2018-12-10 15:54:02 +0000

    Updates

---
+++ precache-manifest.cdc1c78058dca5b8808d01e5e3857c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb1ea6441ac1fd1bbcf8",
+    "url": "/movio/static/js/main.eb1ea644.chunk.js"
+  },
+  {
+    "revision": "38e922058fe9bd8811a9",
+    "url": "/movio/static/css/1.79b8cd5f.chunk.css"
+  },
+  {
+    "revision": "38e922058fe9bd8811a9",
+    "url": "/movio/static/js/1.38e92205.chunk.js"
+  },
+  {
+    "revision": "498f9b83def4d017c23f",
+    "url": "/movio/static/js/runtime~main.498f9b83.js"
+  },
+  {
+    "revision": "eb4c2ec578707ed845844ff4ecafac9d",
+    "url": "/movio/index.html"
+  }
+];
