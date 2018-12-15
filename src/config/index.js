cbdeddd4cffc8c90d49bbb660f9231a25d81eb1b
Salvatore Tedde <microcipcip@gmail.com>
2018-12-15 17:22:35 +0000

    Adding config file

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export const TMDB_API_LANG = 'en-US'
+export const TMDB_API_KEY =
+  process.env.REACT_APP_TMDB_API_KEY || 'mystringkeyhere'
+export const TMDB_API_URL = 'https://api.themoviedb.org/3/'
+export const TMDB_API_IMG_URL = 'https://image.tmdb.org/t/p/'
+export const TMDB_VOTE_RATING_DEFAULT = 3
